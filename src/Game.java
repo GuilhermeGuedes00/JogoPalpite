ed61e1720ceff2d2d279735eafa84b7a4339ebe9
Guilherme Guedes <guilhermeguedes301@gmail.com>
2024-05-11 01:47:04 -0300

    modificação

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Game {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int numeroGerado = new Random().nextInt(100);
+        int tentativas = 0;
+        int numeroDigitado = 0;
+
+        while (tentativas < 5) {
+            System.out.println("Digite um número:");
+            numeroDigitado = leitor.nextInt();
+            tentativas++;
+
+            if (numeroDigitado == numeroGerado) {
+                System.out.println("Parabéns você acertou está misera");
+                break;
+
+            } else if (numeroDigitado < numeroGerado) {
+                System.out.println("!!!O número é maior!!!");
+
+            } else {
+                System.out.println("!!!O número é menor!!!");
+            }
+        }
+        if (tentativas == 5 && numeroDigitado != numeroGerado);
+        System.out.println("Que pena a IA te venceu");
+    }
 }
